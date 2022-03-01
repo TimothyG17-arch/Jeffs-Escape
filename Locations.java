2ebc2483c03382d29e3bcfeee8a75a901b950f74
TimothyG17-arch <71781659+TimothyG17-arch@users.noreply.github.com>
2022-02-28 22:29:20 -0500

    Add files via upload

--- Locations.java
+++ Locations.java
@@ -2,14 +2,17 @@ public class Locations
 {
     private Locations()
     {
-        String area = "";
+        String area = "";//areas in hashmap
 
 
     }
     private void itemLook(){
-
+        //will search room and look for items in the current room
     }
     private void cardLook(){
-
+        //will look for key cards for accessing the next area in current room
+    }
+    private void enemyLook(){
+        //looks for enemies inside current room
     }
 }
