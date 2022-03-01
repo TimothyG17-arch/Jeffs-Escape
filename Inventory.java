c90973ed6e61df9070da8743a3173835390bd6af
davidedwards-12 <91171018+davidedwards-12@users.noreply.github.com>
2022-02-28 22:20:15 -0500

    Add files via upload

--- Inventory.java
+++ Inventory.java
@@ -1,19 +1,20 @@
 public class Inventory
 {
-    private Inventory()
-    {
+    private String[] Inventory = {};
 
-    }
     private void add()
     {
-
+        // Will add items into the character's Inventory
     }
+
     private void remove()
     {
-
+        // Will remove items from the character's Inventory
     }
-    private void printitems()
+
+    private void printItems()
     {
-
+        // Will print the items that are currently in the Inventory
     }
+
 }
