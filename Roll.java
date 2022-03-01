a48265464f4c2d1e6cd36dea29c65ebff78c0bef
Logan R <48109101+loganr82@users.noreply.github.com>
2022-02-28 22:16:45 -0500

    Add files via upload

--- Roll.java
+++ Roll.java
@@ -1,25 +1,19 @@
 import java.util.Random;
 
-public class Roll
+public class Roll // the purpose of roll is for the randomized attack damage done by you and your opponent
 {
     private Roll()
     {
-        int val = 0;
+        int val = 0; // value that will be changed each roll
 
     }
     private void random()
     {
-        Random random = new Random();
+        Random random = new Random(); // random object to assign a number to val
     }
-    private void dice()
+    private void dice() // the dice will have values from 1-20 and have breakpoints at 1-5, 6-10, 11-15, 16-20
+                        // that determine how much damage you or the enemy will do on your attack
     {
 
     }
-    private void quater()
-    {
-
-    }
-
-
-
 }
