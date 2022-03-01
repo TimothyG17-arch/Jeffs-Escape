21f967f940147e421a3c914884d69e7ad85936f7
TimothyG17-arch <71781659+TimothyG17-arch@users.noreply.github.com>
2022-02-28 20:58:43 -0500

    Add files via upload

--- Combat.java
+++ Combat.java
@@ -1,2 +1,22 @@
-public class Combat {
+public class Combat
+{
+    private Combat()
+    {
+        int playerHp = 0;
+        int enemyHp = 0;
+        int playDmg = 0;
+        int enemyDmg = 0;
+    }
+    private void attack()
+    {
+
+    }
+    private void flee()
+    {
+
+    }
+    public void die()
+    {
+        
+    }
 }
