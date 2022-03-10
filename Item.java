4a49658a939e2c9cf07326497d4c0896d0c5ada1
David Edwards <91171018+davidedwards-12@users.noreply.github.com>
2022-03-09 21:12:40 -0500

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,10 +1,39 @@
 public class Item
 {
+    protected String name;
+    protected String description;
 
-    private Item()
+    public Item(String name, String description)
     {
-        String name =  " "; // Shows the name of the item
-        String description = " "; // Gives a brief rundown of what the item is
+        this.name = name;
+        this.description = description;
     }
 
+    // Getters and Setters for name and description
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // toString method to print out name and description
+    public String toString()
+    {
+        String displayString = "";
+
+        displayString += "\nItem Name: " + name + "\n";
+        displayString += "Description: " + description + "\n";
+
+        return displayString;
+    }
 }
