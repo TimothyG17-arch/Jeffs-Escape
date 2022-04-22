efaddbd598978c910388832e7fe3f764237c1b73
TimothyG17-arch <71781659+TimothyG17-arch@users.noreply.github.com>
2022-04-22 19:11:05 -0400

    Delete Weapons.java

--- Weapons.java
+++
@@ -1,8 +0,0 @@
-public class Weapons
-{
-    private Weapons()
-    {
-        String weaponType = ""; // Determines what type of weapon it is from our list of enums
-        int weaponDmg = 0; // how much damage the weapon adds to the characters existing damage
-    }
-}
